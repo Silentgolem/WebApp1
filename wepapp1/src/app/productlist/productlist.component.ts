4f18ad753ffb042a2c913e9357a34836e74e435b
silentgolem <liamlogan16@yahoo.co.uk>
2017-09-21 10:34:50 +0100

    Up to directives done

--- wepapp1/src/app/productlist/productlist.component.ts
+++ wepapp1/src/app/productlist/productlist.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-productlist',
   templateUrl: './productlist.component.html',
   styleUrls: ['./productlist.component.css']
 })
-export class ProductlistComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class ProductlistComponent {
+pageTitle:string="Joe's Products";
+products:any[]=
+[
+  {
+    'productId': 2,
+    'productName': 'Garden Cart',
+    'productCode': 'GDN-0023',
+    'releaseDate': 'March 18, 2016',
+    'description': '15 gallon capacity rolling grarden cart',
+    'price': 32.99,
+    'starRating': 4.2,
+    'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
+  },
+  {
+    'productId': 5,
+    'productName': 'Hammer',
+    'productCode': 'TBX-0048',
+    'releaseDate': 'May 20, 2016',
+    'description': 'Curved claw steel hammer',
+    'price': 8.99,
+    'starRating': 4.8,
+    'imageUrl': 'http://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png'
   }
-
+]
 }
